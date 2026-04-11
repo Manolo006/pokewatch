374be1cb4f14610e982b43560dfb28edb8a2ba3b
Manolo006 <manuelcarpita@gmail.com>
2026-04-11 18:51:26 +0200

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="border-t border-white/10 bg-black/70">
       <div className="mx-auto flex w-full max-w-[1500px] items-center justify-start gap-4 px-3 py-5 text-xs text-white/70 sm:px-8 sm:text-sm">
         <Link href="/" className="shrink-0">
-          <img src="../logo.png" alt="PokéWatch" width={110} height={26} className="h-auto w-[92px] sm:w-[110px]" />
+          <img src="./logo.png" alt="PokéWatch" width={110} height={26} className="h-auto w-[92px] sm:w-[110px]" />
         </Link>
         <p>© 2026 PokéWatch. Tutti i diritti riservati.</p>
         <Link href="/privacy" className="ml-auto underline hover:text-white">
